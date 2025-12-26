f7bd540405be1337132e990faeef6023704da7ec
lovemathboy <lovemathboy@gmail.com>
2025-12-26 11:51:38 +0800

    Change restaurant context to hotel

--- samples/agent/adk/hotel_finder/agent_executor.py
+++ samples/agent/adk/hotel_finder/agent_executor.py
@@ -104,9 +104,9 @@ class HotelAgentExecutor(AgentExecutor):
                 hotel_name = ctx.get("hotelName", "Unknown Hotel")
                 party_size = ctx.get("partySize", "Unknown Size")
                 reservation_time = ctx.get("reservationTime", "Unknown Time")
-                dietary_reqs = ctx.get("dietary", "None")
+                # dietary_reqs = ctx.get("dietary", "None")
                 image_url = ctx.get("imageUrl", "")
-                query = f"User submitted a booking for {hotel_name} for {party_size} people at {reservation_time} with dietary requirements: {dietary_reqs}. The image URL is {image_url}"
+                query = f"User submitted a booking for {hotel_name} for {party_size} people at {reservation_time}. The image URL is {image_url}"
 
             else:
                 query = f"User submitted an event: {action} with data: {ctx}"
